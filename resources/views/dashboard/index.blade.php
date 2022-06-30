f1a9d3e8ca0209f1707735b1487237d450d81e59
Shironee <75158520+PutraNuryansyahAT@users.noreply.github.com>
2022-06-30 14:47:07 +0700

    adding file view

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,25 +7,28 @@
             Hello, Putra Nuryansyah Arsyi Tegara
         </h2>
         <!-- CTA -->
-        <div class="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-orange-500 bg-white-100 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple">
+        <div class="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-orange-500 bg-white-100 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple" ">
             <span>Sahabat Kebaikan Rumah Zakat</span>
             <p> | Sejak 20-20-2000
         </div>
         <!-- Cards -->
-        <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+        <div class=" grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
             <!-- Card -->
-            <div class="flex items-center p-4 bg-white rounded-lg shadow-inner  border-2 hover:border-orange-400 ">
-                <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full ">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
-                    </svg>
+            <a href="#" class="">
+                <div class="flex items-center p-4 bg-white rounded-lg shadow-inner  border-2 hover:border-orange-400 ">
+                    <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full ">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
+                        </svg>
+                    </div>
+
+                    <p class=" font-medium text-gray-600 mb-0">
+                        Link Affiliate
+                    </p>
+
                 </div>
+            </a>
 
-                <p class=" font-medium text-gray-600 mb-0">
-                    Link Affiliate
-                </p>
-
-            </div>
             <!-- Card -->
             <div class="flex items-center p-4 bg-white rounded-lg shadow-inner border-2 hover:border-orange-500 ">
                 <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500">
